4f5027f953d2ed0666a5897875b1a565bc9cb23d
el-racional <esteban.carreno.c@gmail.com>
2019-11-01 17:08:41 -0300

    me falto grabar

--- script_nuclear.R
+++ script_nuclear.R
@@ -1,12 +1,35 @@
-library(rvest); library(dplyr)
+library(RSelenium); library(tidyverse); library(XML); library(rvest)
 
-## just preparte to learn RSelenium for fuck                          
+# NO SE PUEDE USAR RVEST PORQUE NO DEJA ENTRAR AL DETALLE DE CADA PLANTA
+# CON follow_link ni jump_to (páginas dinámicas)
+
+# seteo en link inician
+url <- "https://pris.iaea.org/PRIS/CountryStatistics/CountryDetails.aspx?current=AR"
+
+# cargo los drivers, y abro la sesión (que es básicamente abrir el navegador)
+driver <- rsDriver(browser = c("firefox"))
+remote_driver <- driver[["client"]]
+remote_driver$open()
+
+# voy al link inicial
+remote_driver$navigate(url)
+
+# identifico la tabla que quiero
+tabla <- remote_driver$findElement(using = "class",
+                                   value = "tablesorter")
+
+# le saco el html de adentro, y la parseo a df con comands de Rvest
+front.table <- tabla$getElementAttribute("outerHTML")[[1]] %>%
+  read_html() %>%
+  html_node("table") %>%
+  html_table() %>%
+  as.data.frame()
 
 
 
 
+planta <- remote_driver$findElement(using = "id", 
+                                    value = "MainContent_MainContent_rptCountryReactors_hypReactorName_0")
 
-
-
-
-
+planta$clickElement()
+planta$goBack()
